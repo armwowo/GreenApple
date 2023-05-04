c9647f236194b58963c66019c3de5330ff63f3f7
Aorakin <>
2023-05-04 16:52:48 +0700

    not change anything

--- n-frontend/src/App.js
+++ n-frontend/src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <>
     <Navbar />
-    <div className="container">
+    <div classN ame="container">
       
         <Routes>
           <Route path="/" element={<Home />} />
